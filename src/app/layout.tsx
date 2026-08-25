34d1697b8eaf5e175d14cf76a991b7e67fc7e59a
Imetheran <vekamelgaming@gmail.com>
2026-08-25 20:01:24 +0200

    style: load member authentication interfaces

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,7 @@ import "./forum-access.css";
 import "./forum-thread.css";
 import "./forum-form-focus.css";
 import "./forum-daily-use.css";
+import "./auth.css";
 
 export const metadata: Metadata = {
   title: {
