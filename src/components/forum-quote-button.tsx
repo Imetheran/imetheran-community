e595f2072cf78738c6c9a6e2ede7ffeda2d7acee
Imetheran <vekamelgaming@gmail.com>
2026-08-28 13:00:08 +0200

    fix: label forum quote action

--- src/components/forum-quote-button.tsx
+++ src/components/forum-quote-button.tsx
@@ -14,6 +14,7 @@ export function ForumQuoteButton({
   return (
     <button
       type="button"
+      aria-label={`Citer le message de ${authorName}`}
       onClick={() => {
         window.dispatchEvent(new CustomEvent(FORUM_QUOTE_EVENT, { detail: { quote } }));
         requestAnimationFrame(() => {
